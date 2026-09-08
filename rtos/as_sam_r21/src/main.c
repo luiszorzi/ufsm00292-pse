fbf9e1ef986415a7a5c4e6a2f528b4d4f646f7d0
luiszorzi <luispedro.zorzi3@gmail.com>
2026-09-08 15:15:31 -0300

    Exercicio 01 - adiciona terceira tarefa ao RTOS

--- rtos/as_sam_r21/src/main.c
+++ rtos/as_sam_r21/src/main.c
@@ -81,6 +81,8 @@ int main(int argc, char** argv)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+
+	CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
